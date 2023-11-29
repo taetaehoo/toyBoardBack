3ae18c525045d0d70ee0dcf7e04632bdb60951f3
taetaehoo <hapkldo09@naver.com>
2023-11-29 14:49:13 +0900

    first commit

---
+++ src/main/java/toy/board/toyboardback/ToyboardbackApplication.java
@@ -0,0 +1,13 @@
+package toy.board.toyboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboardbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyboardbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/board/toyboardback/ToyboardbackApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.board.toyboardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboardbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
